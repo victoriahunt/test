ea7cbcb490956aacebe061314ed9ea1ef5a56696
Vicky Hunt <49412554+victoriahunt@users.noreply.github.com>
2019-04-23 13:51:01 -0700

    Uploading v short python script
    Short python script that makes a simple plot, from PyCharm

---
+++ test.py
@@ -0,0 +1,16 @@
+print(1+1)
+
+import matplotlib
+import numpy
+from matplotlib import pyplot as plt
+
+
+days=(1,2,3,4,5,6,7,8,9,10)
+temp = (10,20,15,25,30,35,50,24,28,50)
+plt.plot(days,temp, color='purple')
+plt.title('day vs. temp')
+plt.xlabel('days')
+plt.ylabel('temp')
+plt.show('plottest1.png')
+
+#C:\Users\vhunt\PycharmProjects\untitled\test.py
